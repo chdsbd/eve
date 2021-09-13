061251de4f7161167aa52b064591cad2874c7ff6
Christopher Dignam <chris@dignam.xyz>
2021-09-13 15:01:31 -0400

    migrate to heroku webhooks

---
+++ src/heroku.rs
@@ -0,0 +1,62 @@
+use reqwest::header::{ACCEPT, AUTHORIZATION, RANGE};
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+
+use jsonwebtoken::{Algorithm, EncodingKey, Header};
+use std::time::Duration;
+use std::time::{SystemTime, UNIX_EPOCH};
+
+#[derive(Debug)]
+pub enum HerokuError {
+    HttpError(reqwest::Error),
+}
+
+impl std::convert::From<reqwest::Error> for HerokuError {
+    fn from(e: reqwest::Error) -> Self {
+        Self::HttpError(e)
+    }
+}
+
+#[derive(Deserialize)]
+pub struct HerokuReleaseSlug {
+    pub id: String,
+}
+#[derive(Deserialize)]
+pub struct HerokuRelease {
+    pub slug: HerokuReleaseSlug,
+}
+
+pub fn get_release(app: &str, release: i64, token: &str) -> Result<HerokuRelease, HerokuError> {
+    let res = reqwest::blocking::Client::new()
+        .get(&format!(
+            "https://api.heroku.com/apps/{app}/releases/{release}",
+            app = app,
+            release = release
+        ))
+        .header("User-Agent", "chdsbd/eve")
+        .header(AUTHORIZATION, format!("Bearer {}", token))
+        .header(ACCEPT, "application/vnd.heroku+json; version=3")
+        .header(RANGE, "version; order=desc")
+        .send()?;
+    res.error_for_status_ref()?;
+    Ok(res.json::<HerokuRelease>()?)
+}
+#[derive(Deserialize)]
+pub struct HerokuSlug {
+    pub commit: String,
+}
+
+pub fn get_slug(app: &str, slug: &str, token: &str) -> Result<HerokuSlug, HerokuError> {
+    let res = reqwest::blocking::Client::new()
+        .get(&format!(
+            "https://api.heroku.com/apps/{app}/slugs/{slug}",
+            app = app,
+            slug = slug
+        ))
+        .header("User-Agent", "chdsbd/eve")
+        .header(AUTHORIZATION, format!("Bearer {}", token))
+        .header(ACCEPT, "application/vnd.heroku+json; version=3")
+        .send()?;
+    res.error_for_status_ref()?;
+    Ok(res.json::<HerokuSlug>()?)
+}
--- src/http.rs
+++ src/http.rs
@@ -1,10 +1,84 @@
 use rocket::config::{Config, Environment};
 use rocket::request::LenientForm;
 use rocket::State;
+use rocket_contrib::json::Json;
+use serde::Deserialize;
+
+use crate::heroku;
 
 #[get("/")]
 const fn root() -> &'static str {
     "Heroku Deploy Notifier"
+}
+
+#[derive(Deserialize, Debug)]
+struct WebhookReleaseApp {
+    name: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct WebhookReleaseEventSlug {
+    id: String,
+    commit: String,
+}
+#[derive(Deserialize, Debug)]
+struct WebhookReleaseEventData {
+    app: WebhookReleaseApp,
+    slug: WebhookReleaseEventSlug,
+    version: i64,
+}
+#[derive(Deserialize, Debug)]
+struct WebhookReleaseEvent {
+    action: String,
+    data: WebhookReleaseEventData,
+}
+
+#[post(
+    "/heroku_webhook_hook?<auth_token>&<github_org_name>&<github_repo_name>",
+    data = "<task>"
+)]
+fn heroku_webhook(
+    task: Json<WebhookReleaseEvent>,
+    auth_token: String,
+    github_org_name: String,
+    github_repo_name: String,
+    config: State<crate::cli::Opt>,
+) -> Result<(), crate::EveError> {
+    if auth_token != config.secret {
+        return Err(crate::EveError::InternalError("invalid auth".to_string()));
+    }
+
+    if task.action != "update" {
+        return Ok(());
+    }
+    let release = task.data.version;
+    let app = &task.data.app.name;
+    let heroku_token = "ef3c2122-1f73-47e2-bbbc-73ca71f219ca";
+    let base_ref = heroku::get_slug(
+        app,
+        &heroku::get_release(app, release - 1, heroku_token)?.slug.id,
+        heroku_token,
+    )?
+    .commit;
+
+    let head_ref = &task.data.slug.commit;
+
+    Ok(crate::handle_post_deploy_event(
+        crate::HandlePostDeployEvent {
+            github_app_private_key: &config.github_app_private_key,
+            github_app_id: &config.github_app_id,
+            github_app_install_id: &config.github_app_install_id,
+            github_org: &github_org_name,
+            github_repo: &github_repo_name,
+            github_ref_base: &base_ref,
+            github_ref_head: head_ref,
+            github_slack_users: &config.github_slack_user_ids,
+            slack_oauth_token: &config.slack_oauth_token,
+            heroku_release: &format!("v{}", release),
+            heroku_app_name: app,
+            now: chrono::Utc::now().into(),
+        },
+    )?)
 }
 
 // https://devcenter.heroku.com/articles/deploy-hooks#http-post-hook
@@ -57,7 +131,7 @@ pub fn start_server(opt: crate::cli::Opt) {
     let mut config = Config::new(env);
     config.port = opt.port;
     rocket::custom(config)
-        .mount("/", routes![root, heroku_deploy_hook])
+        .mount("/", routes![root, heroku_deploy_hook, heroku_webhook])
         .manage(opt)
         .launch();
 }
